97cf2d7c5031300c6c6f6a0a71f43fee42aa4fbf
Guwanjith Tennekoon <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-11 01:06:54 +0530

    Added in Gameplay  - now can shoot penguins (basic)

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Guwanjith Tennekoon on 9/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
